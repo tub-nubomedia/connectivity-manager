e6d66473ff33ad535d25a6026dfd9aa6a1e73e0f
Pasquale Maiorano Picone <pasquale.maiorano.picone@fokus.frauhofer.de>
2015-12-15 19:06:07 +0100

    queue choosed

--- src/main/java/org/nubomedia/qosmanager/beans/connectivitymanager/FlowHandler.java
+++ src/main/java/org/nubomedia/qosmanager/beans/connectivitymanager/FlowHandler.java
@@ -52,7 +52,7 @@ public class FlowHandler {
                             tmp.setPriority(priority);
                             tmp.setProtocol(protocol);
                             tmp.setSrc_ipv4(iface.getIp());
-                            tmp.setQueue_number("" + iface.getQos().getActualID());
+                            tmp.setQueue_number("" + iface.getQos().getActualID()+1);
                             internalFlows.add(tmp);
                         }
                         fs.setQos_flows(internalFlows);
